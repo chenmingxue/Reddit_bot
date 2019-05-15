0791cb2faf1ce8ca8db8e4d1e9a4d4445b9db283
ChenmingXue <chenming.xue2011@gmail.com>
2019-05-14 18:20:32 -0700

    Update reddit_bot.py

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,9 +1,9 @@
 import praw
 #import config
-reddit=praw.Reddit("bot1")
+reddit=praw.Reddit("bot1") #the bot1 is setup in ini file with personal reddit account information
 subreddit=reddit.subreddit("learnpython")
 print ("load successful!")
-for submission in subreddit.hot(limit=5):
+for submission in subreddit.hot(limit=5): #print information in terminal
     print("Title: ", submission.title)
     print("Text: ", submission.selftext)
     print("Score: ", submission.score)
